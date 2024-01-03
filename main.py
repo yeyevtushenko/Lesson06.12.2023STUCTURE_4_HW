e4aada5e55be9daa126a8d1a9a2ad21e8a278e8d
yeyevtushenko <yehor803@gmail.com>
2024-01-03 16:09:10 +0200

    Ініціалізація класу особа з функціями додавання/видалення штрафу, оновленням інформації та рядковим виведенням

--- main.py
+++ main.py
@@ -7,3 +7,25 @@ class Penalty:
     def __str__(self):
         return f"Тип: {self.penalty_type}, Сума: {self.amount}, Місто: {self.city}"
 
+class Person:
+    def __init__(self, personal_code, name, city):
+        self.personal_code = personal_code
+        self.name = name
+        self.city = city
+        self.penalties = []
+
+    def add_penalty(self, penalty):
+        self.penalties.append(penalty)
+
+    def remove_penalty(self, penalty):
+        self.penalties.remove(penalty)
+
+    def update_info(self, name, city):
+        self.name = name
+        self.city = city
+
+    def __str__(self):
+        penalties_info = "\n".join([str(penalty) for penalty in self.penalties])
+        return f"Персональний код: {self.personal_code}\nІм'я: {self.name}\nМісто: {self.city}\nШтрафи:\n{penalties_info}"
+
+
