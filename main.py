06d1296955e05823f25b69b2fb3ad24889230f1a
yeyevtushenko <yehor803@gmail.com>
2024-01-03 16:16:09 +0200

    Ініціалізація класу база даних податкової з фінкуціями схожими в класі особа

--- main.py
+++ main.py
@@ -28,4 +28,55 @@ class Person:
         penalties_info = "\n".join([str(penalty) for penalty in self.penalties])
         return f"Персональний код: {self.personal_code}\nІм'я: {self.name}\nМісто: {self.city}\nШтрафи:\n{penalties_info}"
 
+class TaxInspectDB:
+    def __init__(self):
+        self.people = {}
+
+    def add_person(self, person):
+        self.people[person.personal_code] = person
+
+    def remove_person(self, personal_code):
+        if personal_code in self.people:
+            del self.people[personal_code]
+
+    def add_penalty(self, personal_code, penalty):
+        if personal_code in self.people:
+            person = self.people[personal_code]
+            person.add_penalty(penalty)
+
+    def remove_penalty(self, personal_code, penalty):
+        if personal_code in self.people:
+            person = self.people[personal_code]
+            person.remove_penalty(penalty)
+
+    def update_person_info(self, personal_code, name, city):
+        if personal_code in self.people:
+            person = self.people[personal_code]
+            person.update_info(name, city)
+
+    def print_db(self):
+        for personal_code, person in self.people.items():
+            print(f"Особа з персональним кодом {personal_code}:\n{person}\n")
+
+    def print_by_code(self, personal_code):
+        if personal_code in self.people:
+            person = self.people[personal_code]
+            print(f"Особа з персональним кодом {personal_code}:\n{person}\n")
+        else:
+            print(f"Особу не знайдено за таким персональним кодом {personal_code}")
+
+    def print_by_penalty_type(self, penalty_type):
+        for personal_code, person in self.people.items():
+            penalties = [penalty for penalty in person.penalties if penalty.penalty_type == penalty_type]
+            if penalties:
+                print(f"Особа з персональним кодом {personal_code} має штраф за  {penalty_type}:\n")
+                for penalty in penalties:
+                    print(penalty)
+                print("\n")
+
+    def print_by_city(self, city):
+        for personal_code, person in self.people.items():
+            if person.city == city:
+                print(f"Особа з персональним кодом{personal_code} живе в  {city}:\n{person}\n")
+
 
